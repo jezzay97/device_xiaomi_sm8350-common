c26e06268e05439c7b18ee4d5d32a63c60e2e649
jessepincay <pincayjesse@gmail.com>
2024-05-15 15:08:28 -0500

    Welcome BlissROM

--- common.mk
+++ common.mk
@@ -435,7 +435,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pixelstar
+    $(LOCAL_PATH)/overlay-bliss
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
